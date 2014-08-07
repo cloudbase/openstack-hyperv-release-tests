c15e8c810209b6bc3521d4cb57f9516b42543f4c
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-08-07 05:57:36 -0700

    Adds Hyper-V Nova restart script

---
+++ restartnova.ps1
@@ -0,0 +1,11 @@
+net stop nova-compute
+net stop neutron-hyperv-agent
+net stop ceilometer-agent-compute
+
+stop-vm instance-* -Force -Passthru | Remove-Vm -Force
+rmdir C:\OpenStack\Instances -Recurse -Force
+del C:\OpenStack\Log\*
+
+net start nova-compute
+net start neutron-hyperv-agent
+net start ceilometer-agent-compute
