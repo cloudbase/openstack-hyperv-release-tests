4a619f67bcf560d6ab9d94f9436c890012f6ea7a
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-08-17 14:33:15 +0300

    Updates devstack local scripts

--- local.sh
+++ local.sh
@@ -7,10 +7,12 @@ export OS_TENANT_NAME=admin
 export OS_AUTH_URL=http://127.0.0.1:5000/v2.0
 
 nova flavor-delete 42
-nova flavor-create m1.nano 42 256 3 1
+nova flavor-create m1.nano 42 64 1 1
+#nova flavor-create m1.nano 42 256 3 1
 
 nova flavor-delete 84
-nova flavor-create m1.micro 84 300 4 1
+nova flavor-create m1.micro 84 128 2 1
+#nova flavor-create m1.micro 84 300 4 1
 
 nova flavor-delete 451
 nova flavor-create m1.heat 451 512 5 1
