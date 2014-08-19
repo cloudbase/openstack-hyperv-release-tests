6a54080890821501ec245741134739d8ca7d2bae
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-08-20 02:40:33 +0300

    Adds live migration scripts

---
+++ setkdc.ps1
@@ -0,0 +1,4 @@
+.\Set-KCD.ps1 NUCHV12R201 NUCHV12R202 -ServiceType "Microsoft Virtual System Migration Service"
+.\Set-KCD.ps1 NUCHV12R201 NUCHV12R202 -ServiceType cifs
+.\Set-KCD.ps1 NUCHV12R202 NUCHV12R201 -ServiceType "Microsoft Virtual System Migration Service"
+.\Set-KCD.ps1 NUCHV12R202 NUCHV12R201 -ServiceType cifs
