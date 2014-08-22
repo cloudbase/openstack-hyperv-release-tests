a71d600f37bbe3b1f3fb02714578b8ef66b4bbf0
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-08-22 16:26:46 +0300

    Adds nova startup script

---
+++ nova.cmd
@@ -0,0 +1,2 @@
+ "C:\Program Files (x86)\Cloudbase Solutions\OpenStack\Nova\Python27\Scripts\nova-compute.exe" --config-file "C:\Program Files (x86)\Cloudbase Solutions\OpenStack\Nova\etc\nova.conf"
+ 
