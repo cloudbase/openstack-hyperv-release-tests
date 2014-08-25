5aae689e17c5c06bf09165e399c430d3913e9932
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-08-25 15:56:10 +0300

    Improves the 2008 R2 trunked VLANs workaround

--- trunked_vlans_workaround_2008r2.ps1
+++ trunked_vlans_workaround_2008r2.ps1
@@ -1,7 +1,27 @@
 $ErrorActionPreference = "Stop"
 
 $ns = "root\virtualization"
-$p = (gwmi -Namespace $ns -class Msvm_VLANEndpointSettingData -Filter "elementname = 'ExternalSwitchPort'")
-$p.TrunkedVLANList = @()
-for ($i=500; $i -le 2000; $i++) { $p.TrunkedVLANList += $i }
-$p.Put()
+
+$sw = gwmi -Namespace $ns -class "Msvm_virtualswitch" -Filter "ElementName = 'external'"
+$swports = gwmi -Namespace $ns -q "associators of {$sw} where ResultClass=Msvm_SwitchPort"
+foreach($swport in $swports) {
+    $swleps = gwmi -Namespace $ns -q "associators of {$swport} where ResultClass=Msvm_SwitchLanEndPoint"
+    if($swleps) {
+        foreach($swlep in $swleps) {
+            $eeps = gwmi -Namespace $ns -q "associators of {$swlep} where ResultClass=Msvm_ExternalEthernetPort"
+            if($eeps) {
+                $vlanep = gwmi -Namespace $ns -q "associators of {$swport} where assocClass=MSVM_Bindsto"
+                if ($vlanep.DesiredEndpointMode -ne 5 ) {
+                    $vlanep.DesiredEndpointMode = 5
+                    $vlanep.Put()
+                }                
+                $vlanep= gwmi -Namespace $ns -q "associators of {$vlanep} where ResultClass=Msvm_VLANEndpointSettingData"                
+                $vlanep.TrunkedVLANList = @()
+                for ($i=500; $i -le 2000; $i++) { 
+                    $vlanep.TrunkedVLANList += $i
+                }
+                $vlanep.Put()                
+            }
+        }
+    }
+}
