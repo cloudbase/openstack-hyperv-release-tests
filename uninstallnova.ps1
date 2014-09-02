9abaaee2a0085472cf88a3512a529ab78f003b94
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-09-02 19:59:43 +0300

    Fixes bug in nova uninstall script

--- uninstallnova.ps1
+++ uninstallnova.ps1
@@ -1,12 +1,13 @@
 $ErrorActionPreference = "Stop"
 
 $products = gwmi Win32_Product -filter "Vendor = 'Cloudbase Solutions Srl'" | where {$_.Caption.StartsWith('OpenStack Hyper-V Nova Compute')}
-
-foreach($product in $products) {
-    Write-Host "Uninstalling ""$($product.Caption)"""
-    $p = Start-Process -Wait "msiexec.exe" -ArgumentList "/uninstall $($product.IdentifyingNumber) /qn /l*v log_uninstall.txt" -PassThru
-    if($p.ExitCode) { throw 'Uninstalling "$($product.Caption)" failed'}
-    Write-Host """$($product.Caption)"" uninstalled successfully"
+if ($products) {
+    foreach($product in $products) {
+        Write-Host "Uninstalling ""$($product.Caption)"""
+        $p = Start-Process -Wait "msiexec.exe" -ArgumentList "/uninstall $($product.IdentifyingNumber) /qn /l*v log_uninstall.txt" -PassThru
+        if($p.ExitCode) { throw 'Uninstalling "$($product.Caption)" failed'}
+        Write-Host """$($product.Caption)"" uninstalled successfully"
+    }
 }
 
 $appPath = "${ENV:ProgramFiles(x86)}\Cloudbase Solutions\OpenStack\Nova"
