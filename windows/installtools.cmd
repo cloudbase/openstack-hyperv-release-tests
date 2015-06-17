de5c851498649bcc27cc0bb2beceb945e74abc58
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2015-06-17 03:33:30 +0300

    Adds firewall rules to installtools.cmd

--- windows/installtools.cmd
+++ windows/installtools.cmd
@@ -4,3 +4,6 @@ cinst notepadplusplus.install
 cinst git.install
 
 setx /m PATH "%PATH%;c:\Program Files (x86)\Git\bin;c:\Program Files (x86)\Notepad++"
+
+netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=yes
+netsh advfirewall firewall set rule group="Remote Desktop" new enable=Yes
