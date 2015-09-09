235572c6f4b3e27c47e5eef6a340e4a57b9e67f2
Claudiu B. <bclau@users.noreply.github.com>
2015-09-09 17:31:31 +0300

    Adds details to the errors raised by windows/ini.psm1
    Adds details about the key / values if the operation failed.

--- windows/ini.psm1
+++ windows/ini.psm1
@@ -74,7 +74,7 @@ function Get-IniFileValue
             $lastErr = [PSCloudbase.Win32IniApi]::GetLastError()
             if ($lastErr -ne 2)
             {
-                throw "Cannot get value from ini file: " + [PSCloudbase.Win32IniApi]::GetLastError()
+                throw "Cannot get value for '" + $Key + "' from ini file: " + [PSCloudbase.Win32IniApi]::GetLastError()
             }
             elseif (!(Test-Path $Path))
             {
@@ -107,7 +107,7 @@ function Set-IniFileValue
         $retVal = [PSCloudbase.Win32IniApi]::WritePrivateProfileString($Section, $Key, $Value, $Path)
         if (!$retVal -and [PSCloudbase.Win32IniApi]::GetLastError())
         {
-            throw "Cannot set value in ini file: " + [PSCloudbase.Win32IniApi]::GetLastError()
+            throw "Cannot set key '" + $Key + "' to '" + $Value + "' in ini file: " + [PSCloudbase.Win32IniApi]::GetLastError()
         }
     }
 }
@@ -131,7 +131,7 @@ function Remove-IniFileValue
         $retVal = [PSCloudbase.Win32IniApi]::WritePrivateProfileString($Section, $Key, $null, $Path)
         if (!$retVal -and [PSCloudbase.Win32IniApi]::GetLastError())
         {
-            throw "Cannot remove value from ini file: " + [PSCloudbase.Win32IniApi]::GetLastError()
+            throw "Cannot remove value for '" + $Key + "' from ini file: " + [PSCloudbase.Win32IniApi]::GetLastError()
         }
     }
 }
